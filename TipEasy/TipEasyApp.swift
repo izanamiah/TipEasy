0615be9cea275765a64a04560254cf303e999a03
izanamiah <jeremiah@liuli.studio>
2023-03-04 22:04:30 -0500

    Initial Commit

---
+++ TipEasy/TipEasyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipEasyApp.swift
+//  TipEasy
+//
+//  Created by Jeremiah Liu on 3/4/23.
+//
+
+import SwiftUI
+
+@main
+struct TipEasyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
